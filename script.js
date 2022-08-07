4af223bff393e6c4fbb36a633c4208f819af9493
Tato <tatijavakhishvili@gmail.com>
2022-08-07 05:18:09 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+// 1.
+
+// function sum(...massive) {
+//     let numbersSum = 0;
+//     for (let item of massive) {
+//         if(item > 0)
+//         numbersSum = numbersSum + item;
+//     }
+//     return numbersSum;
+// }
+
+// let result = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(result);
+
+
+// 2.
+
+// function sum(...massive) {
+//     let numbers = 0;
+//     for (let item of massive) {
+//         numbers = numbers + item;
+//     }
+//     return numbers;
+// }
+
+// let result = sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(result);
+
+// 3.
+// function N3(){
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+
+// for (let item of user){
+//     if(isloggedin == 'true') {
+//         return 'giorgi saakadze';
+//     }else if (isloggedin == 'false'){
+//         return 'false';
+//     }
+// }
+// }
+
+// console.log(N3());      // i tryed but failed :( next time ill get it right :)
+
+
+
+//4.
+// function sum(...massive) {
+//     let numbersSum = 468;
+//     for (let item of array) {
+//             numbersSum < item;
+//         }
+//     }
+
+// let result = sum(2, 56, 565, 34, 367, 467, 2056);
+// console.log(result)
+
+
+// 5. 
+
+
+// let array = [1,2,4,10,34,5,7,87,-2];
+
+// for (let item of array) {
+//     if(item >0 && item < 10){
+//         console.log(item);
+//     }
+// }
+
+// 6.
+
+// let numbers =[1,2,3,4,5,6,7,8,9,10];
+
+// for (let index = 0; index < numbers.length; index++) {
+//     const element = numbers[index];
+//     if (element == 5) {
+//         break;
+//     }
+//     console.log(element);
+// }
